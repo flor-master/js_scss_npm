6af0e3e632cc88d1266149aa2426fa1a18cad75d
flor <flor@flors-MacBook-Pro.local>
2016-03-21 21:57:00 +0200

    - first commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    entry: "./src/js/app.js",
+    output: {
+        path: "./dist/js/",
+        filename: "app.js"
+    },
+    module: {
+        loaders: [{
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            query: {
+                presets: 'es2015'
+            }
+        }]
+    }
+};
